b308b5fe284b894c8add2c4e4ec0053ac279cc71
Luan Simões <luaan.carlos@hotmail.com>
2022-06-10 19:18:22 -0300

    att

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -2,7 +2,7 @@ import { Welcome } from '../Welcome';
 import { AboutMe } from '../AboutMe';
 import { Portfolio } from '../Portflio';
 import { Skills } from '../Skills';
-import { StudyWork } from '../StudyWork';
+import { StudyWork } from '../studyWork';
 
 
 import { Container } from './styles';
